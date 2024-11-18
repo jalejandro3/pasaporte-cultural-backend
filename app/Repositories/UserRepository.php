ee3492ec06faf7f39fa3a7f274d49ab4302fce01
Jose Alejandro Alvarez Mendoza <josealejandro.alvarez848@comunidadunir.net>
2024-11-17 23:47:16 -0500

    PCU-8: Eliminar salto de linea

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -7,7 +7,6 @@ use App\Models\User;
 interface UserRepository
 {
     public function create(array $data): bool;
-
     public function findByEmail(string $email): ?User;
     public function findByIdDocument(string $idDocument): ?User;
 }
