b977ff1908d922d8e5375e0a1e2c6deab2c0c27d
Jose Alejandro Alvarez Mendoza <josealejandro.alvarez848@comunidadunir.net>
2024-11-22 21:29:22 -0500

    PCU-15: Crear metodo error

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
+use Symfony\Component\HttpFoundation\Response;
 
 class Controller extends BaseController
 {
@@ -19,5 +20,10 @@ class Controller extends BaseController
     public function success($data): JsonResponse
     {
         return response()->json(['data' => $data]);
+    }
+
+    public function error(string $data, int $status = Response::HTTP_BAD_REQUEST): JsonResponse
+    {
+        return response()->json(['error' => $data], $status);
     }
 }
