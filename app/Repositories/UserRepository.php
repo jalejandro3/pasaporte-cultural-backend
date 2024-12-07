d7b38c9d417155913ceaae5f8636c658006a4aa4
Jose Alejandro Alvarez Mendoza <josealejandro.alvarez848@comunidadunir.net>
2024-12-06 23:11:17 -0500

    PCU-9: Refactorizar metodo para retornar un objeto User

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -6,7 +6,7 @@ use App\Models\User;
 
 interface UserRepository
 {
-    public function create(array $data): bool;
+    public function create(array $data): User;
     public function findById(int $id): ?User;
     public function findByEmail(string $email): ?User;
     public function findByIdDocument(string $idDocument): ?User;
