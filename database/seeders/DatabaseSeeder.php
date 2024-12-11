2dfff99822c927b5bdf6e484769e36269bbeeb13
Diego Espitia <diegoalexander91@hotmail.com>
2024-12-11 09:11:44 -0500

    PCU-25: Ruta ajustada al middleware JWT. Se elimina el seeder de usuario de test

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(AdminUserSeeder::class);
-        $this->call(TestUserSeeder::class);
-
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
--- routes/api.php
+++ routes/api.php
@@ -28,32 +28,9 @@ Route::middleware('jwt')->group(function () {
      * USERS
      */
     Route::get('users/profile', [UserController::class, 'profile']);
+    Route::put('users/profile', [UserController::class, 'updateProfile']);
     Route::middleware('admin.user')->group(function () {
         Route::put('users/{id}', [UserController::class, 'update']);
         Route::delete('users/{id}', [UserController::class, 'destroy']);
     });
 });
-
-/**
- * Modificar perfil
- */
-Route::middleware('auth:sanctum')->put('/updateProfile', [UserController::class, 'updateProfile']);
-
-
-/**
- * Prueba de middlware
- */
-Route::middleware('auth:sanctum')->get('/test', function () {
-    return response()->json(['message' => 'Authenticated!']);
-});
-
-/**
- * Fin de prueba de middlware
- */
-
-Route::get('/genera-token', function () {
-    $user = \App\Models\User::where('id_document', operator: '2233445566')->first(); // Selecciona el usuario de test creado en el seeder
-    $token = $user->createToken('TestToken')->plainTextToken; 
-    return response()->json(['token' => $token]);
-});
-
