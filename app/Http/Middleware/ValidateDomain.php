a1b6a0204bec93a51101782c6b5467e2e0c305a8
Jose Alejandro Alvarez Mendoza <josealejandro.alvarez848@comunidadunir.net>
2025-01-18 20:49:52 -0500

    PCU-45: Validar que el email sea un campo enviado en el request

--- app/Http/Middleware/ValidateDomain.php
+++ app/Http/Middleware/ValidateDomain.php
@@ -22,7 +22,7 @@ class ValidateDomain
             return response()->json(['message' => 'No valid domains were configured.'], Response::HTTP_BAD_REQUEST);
         }
 
-        if (!in_array(extract_domain($email), $allowedDomains)) {
+        if ($email && !in_array(extract_domain($email), $allowedDomains)) {
             return response()->json(['message' => 'Invalid data. Please try again.'], Response::HTTP_BAD_REQUEST);
         }
 
