23e0c68241b0be36a3ffb4c30cb190a4cda7b9ac
Jose Alejandro Alvarez Mendoza <alvarez.josealejandro3@gmail.com>
2026-04-24 12:00:27 -0500

    Skipping test in progress

--- tests/Unit/Application/User/ChangeUserRoleTest.php
+++ tests/Unit/Application/User/ChangeUserRoleTest.php
@@ -8,6 +8,7 @@ class ChangeUserRoleTest extends TestCase
 {
     public function test_change_user_assistant_to_admin_role()
     {
-
+        //In progress
+        $this->markTestSkipped();
     }
 }
